c235962b9445ab2b673fcbd8f8c2ce8cbad6ed5d
Alvaro <osses.alvaro.7@gmail.com>
2023-09-12 17:21:21 -0500

    Fin de seccion 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { counterModule } from './counter/counter.module';
+import { heroesModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    counterModule,
+    heroesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
